e964cf51bffde1dc553bcb9395cc37b59aa89676
allen <frazeeaw@vcu.edu>
2025-03-03 18:42:54 -0500

    added new sounds, buttons, colors, and header

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 // create array of sounds
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const sounds = ['amongus', 'pluh', 'yeahboi', 'ninjafortnite', 'pedro', 'anotherone', 'scoobydoonooo', 'evilhehe', 'brothereww', 'scaryroar', 'minecraftsheep', 'canwegetmuchhigher', 'franklinringtone'];
 
 // for loop
 sounds.forEach(sound => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
     btn.innerText = sound;
+
+    btn.addEventListener('click', () => {
+        stopSounds();
+        document.getElementById(sound).play();
+    });
+
     document.getElementById('buttons').appendChild(btn);
 });
+
+function stopSounds() {
+    sounds.forEach((sound) => {
+        const snd = document.getElementById(sound);
+
+        snd.pause();
+        snd.currentTime = 0;
+    });
+}
